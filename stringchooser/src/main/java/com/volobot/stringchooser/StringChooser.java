0eb51442e7d3c9a701285e8696de56b6557e5f6f
GurjapSingh <sjapgurjap@gmail.com>
2019-06-24 19:41:10 +0530

    removed android x support

--- stringchooser/src/main/java/com/volobot/stringchooser/StringChooser.java
+++ stringchooser/src/main/java/com/volobot/stringchooser/StringChooser.java
@@ -2,16 +2,16 @@ package com.volobot.stringchooser;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.support.annotation.ColorInt;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.ViewTreeObserver;
 
 
-import androidx.annotation.ColorInt;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
--- stringchooser/src/main/java/com/volobot/stringchooser/StringChooserAdapter.java
+++ stringchooser/src/main/java/com/volobot/stringchooser/StringChooserAdapter.java
@@ -1,15 +1,13 @@
 package com.volobot.stringchooser;
 
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
